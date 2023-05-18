e1395e5b02a5dd3ee2508ed900a6cea2ecdeb49f
dkifashion <dkiwomanfashion@gmail.com>
2023-05-18 18:41:59 +0700

    create app constant variable

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/constant/AppConstant.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/constant/AppConstant.java
@@ -1,2 +1,14 @@
-package t3h.android.elifeadmin.constant;public class AppConstant {
+package t3h.android.elifeadmin.constant;
+
+public class AppConstant {
+    public static final String DASHBOARD = "Dashboard";
+    public static final String CREATE_NEW_CATEGORY = "Create new category";
+    public static final String CREATE_NEW_TOPIC = "Create new topic";
+    public static final String CREATE_NEW_AUDIO = "Create new audio";
+    public static final String NEW_CATEGORY = "New category";
+    public static final String NEW_TOPIC = "New topic";
+    public static final String NEW_AUDIO = "New audio";
+    public static final String SEARCH_CATEGORY = "Search category";
+    public static final String SEARCH_TOPIC = "Search topic";
+    public static final String SEARCH_AUDIO = "Search audio";
 }
