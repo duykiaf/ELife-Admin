9847726eb68161fc86a3cc5f7adee68b955b0498
dkifashion <dkiwomanfashion@gmail.com>
2023-05-19 09:01:41 +0700

    handle sign in, forgot password, change password, sign out using firebase authentication

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/helper/FirebaseAuthHelper.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/helper/FirebaseAuthHelper.java
@@ -1,2 +1,42 @@
-package t3h.android.elifeadmin.helper;public class FirebaseAuthHelper {
+package t3h.android.elifeadmin.helper;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
+public class FirebaseAuthHelper {
+    private static final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    public static FirebaseUser getCurrentUser() {
+        return firebaseAuth.getCurrentUser();
+    }
+
+    public static void signIn(String email, String password, OnCompleteListener<AuthResult> listener) {
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(listener);
+    }
+
+    public static void forgotPassword(String email, OnCompleteListener<Void> listener) {
+        firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(listener);
+    }
+
+    public static void changePassword(String oldPassword, String newPassword, OnCompleteListener<Void> listener) {
+        FirebaseUser firebaseUser = getCurrentUser();
+        AuthCredential credential = EmailAuthProvider.getCredential(Objects.requireNonNull(firebaseUser.getEmail()), oldPassword);
+        firebaseUser.reauthenticate(credential).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                firebaseUser.updatePassword(newPassword).addOnCompleteListener(listener);
+            } else {
+                listener.onComplete(task);
+            }
+        });
+    }
+
+    public static void signOut() {
+        firebaseAuth.signOut();
+    }
 }
