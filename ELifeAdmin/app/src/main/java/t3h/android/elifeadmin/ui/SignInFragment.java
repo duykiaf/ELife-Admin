0870a3da338b475b13c06759573a1f3162b2e527
dkifashion <dkiwomanfashion@gmail.com>
2023-05-19 16:05:17 +0700

    create and navigate from sign in fragment to forgot pass fragment

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/ui/SignInFragment.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/ui/SignInFragment.java
@@ -2,19 +2,43 @@ package t3h.android.elifeadmin.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import t3h.android.elifeadmin.R;
+import t3h.android.elifeadmin.databinding.FragmentSignInBinding;
 
 public class SignInFragment extends Fragment {
+    private FragmentSignInBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+        binding =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_sign_in, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.navHostFragment);
+        binding.forgotPwdTxt.setOnClickListener(v -> {
+            navController.navigate(R.id.action_signInFragment_to_forgotPasswordFragment);
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
