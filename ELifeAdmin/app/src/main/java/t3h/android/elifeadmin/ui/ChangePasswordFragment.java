81567b833529cf16fd3f3cade1d2063778a1e137
dkifashion <dkiwomanfashion@gmail.com>
2023-05-19 16:06:15 +0700

    set on back pressed to previous fragment

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/ui/ChangePasswordFragment.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/ui/ChangePasswordFragment.java
@@ -2,6 +2,9 @@ package t3h.android.elifeadmin.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,12 +12,30 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import t3h.android.elifeadmin.R;
+import t3h.android.elifeadmin.databinding.FragmentChangePasswordBinding;
 
 public class ChangePasswordFragment extends Fragment {
+    private FragmentChangePasswordBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_change_password, container, false);
+        binding =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_change_password, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.backTxt.setOnClickListener(v -> {
+            requireActivity().onBackPressed();
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
