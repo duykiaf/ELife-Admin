4e5ae5844a05cdea45d8aa5e8f55fbe4a3a47b8d
dkifashion <dkiwomanfashion@gmail.com>
2023-05-19 15:09:16 +0700

    delete navigation logic when click app bar item

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/ui/AppBarFragment.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/ui/AppBarFragment.java
@@ -4,17 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import t3h.android.elifeadmin.R;
-import t3h.android.elifeadmin.constant.AppConstant;
 import t3h.android.elifeadmin.databinding.FragmentAppBarBinding;
 
 public class AppBarFragment extends Fragment {
@@ -34,25 +30,6 @@ public class AppBarFragment extends Fragment {
         Bundle args = requireArguments();
         appBarBinding.topAppBar.setTitle(args.getString("PAGE_TITLE", "No page found"));
         appBarBinding.topAppBar.setNavigationIcon(args.getInt("PAGE_ICON"));
-
-        if (!appBarBinding.topAppBar.getTitle().equals(AppConstant.DASHBOARD)) {
-            appBarBinding.topAppBar.setNavigationOnClickListener(v -> {
-                requireActivity().onBackPressed();
-            });
-        }
-
-        NavController navController = Navigation.findNavController(requireActivity(), R.id.navHostFragment);
-        appBarBinding.topAppBar.setOnMenuItemClickListener(menuItem -> {
-            switch (menuItem.getItemId()) {
-                case R.id.changePasswordItem:
-                    navController.navigate(R.id.action_dashboardFragment_to_changePasswordFragment);
-                    return true;
-                case R.id.logoutItem:
-                    Toast.makeText(requireActivity(), "Logout!", Toast.LENGTH_SHORT).show();
-                    return true;
-            }
-            return false;
-        });
     }
 
     @Override
