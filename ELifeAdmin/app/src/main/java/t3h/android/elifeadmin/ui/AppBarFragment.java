92e39a274eea8a4f601c1f074e1c7d53065b44cb
dkifashion <dkiwomanfashion@gmail.com>
2023-05-19 10:49:58 +0700

    set navigation when click icon and menu item

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/ui/AppBarFragment.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/ui/AppBarFragment.java
@@ -1,17 +1,20 @@
 package t3h.android.elifeadmin.ui;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import t3h.android.elifeadmin.R;
+import t3h.android.elifeadmin.constant.AppConstant;
 import t3h.android.elifeadmin.databinding.FragmentAppBarBinding;
 
 public class AppBarFragment extends Fragment {
@@ -31,6 +34,25 @@ public class AppBarFragment extends Fragment {
         Bundle args = requireArguments();
         appBarBinding.topAppBar.setTitle(args.getString("PAGE_TITLE", "No page found"));
         appBarBinding.topAppBar.setNavigationIcon(args.getInt("PAGE_ICON"));
+
+        if (!appBarBinding.topAppBar.getTitle().equals(AppConstant.DASHBOARD)) {
+            appBarBinding.topAppBar.setNavigationOnClickListener(v -> {
+                requireActivity().onBackPressed();
+            });
+        }
+
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.navHostFragment);
+        appBarBinding.topAppBar.setOnMenuItemClickListener(menuItem -> {
+            switch (menuItem.getItemId()) {
+                case R.id.changePasswordItem:
+                    navController.navigate(R.id.action_dashboardFragment_to_changePasswordFragment);
+                    return true;
+                case R.id.logoutItem:
+                    Toast.makeText(requireActivity(), "Logout!", Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+            return false;
+        });
     }
 
     @Override
