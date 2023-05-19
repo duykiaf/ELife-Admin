7dd78da700b4a0b528f0c768859b509626ef617e
dkifashion <dkiwomanfashion@gmail.com>
2023-05-19 10:49:13 +0700

    create class to handle click event

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/helper/EventHandler.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/helper/EventHandler.java
@@ -1,2 +1,12 @@
-package t3h.android.elifeadmin.helper;public class EventHandler {
+package t3h.android.elifeadmin.helper;
+
+import android.app.Activity;
+import android.content.Context;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import t3h.android.elifeadmin.R;
+
+public class EventHandler {
 }
