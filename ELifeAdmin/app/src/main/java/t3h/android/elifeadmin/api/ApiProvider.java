fa93bd67c341c9724c6026838419798ff89d9a5c
dkifashion <dkiwomanfashion@gmail.com>
2023-05-29 14:49:18 +0700

    add the header for all api

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/api/ApiProvider.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/api/ApiProvider.java
@@ -1,8 +1,13 @@
 package t3h.android.elifeadmin.api;
 
+import android.content.Context;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import t3h.android.elifeadmin.helper.SharedPreferencesHelper;
 
 public class ApiProvider {
     public static final String BASE_URL = "http://192.168.1.6:3000";
@@ -11,6 +16,12 @@ public class ApiProvider {
     private static TopicApi topicApi;
     private static AudioApi audioApi;
     private static Retrofit retrofit;
+    private static Context context;
+    private static OkHttpClient okHttpClient;
+
+    public ApiProvider(Context context) {
+        this.context = context;
+    }
 
     public static TokenApi getTokenApi() {
         if (tokenApi == null) {
@@ -41,14 +52,29 @@ public class ApiProvider {
     }
 
     public static Retrofit getRetrofit() {
-        if (retrofit == null) {
-            OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
-            retrofit = new Retrofit.Builder()
-                    .client(okHttpClient)
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+        if (context != null) {
+            Interceptor interceptor = chain -> {
+                Request request = chain.request();
+                Request.Builder builder = request.newBuilder();
+                builder.addHeader("x-access-token", SharedPreferencesHelper.getAccessToken(context));
+                return chain.proceed(builder.build());
+            };
+            okHttpClient = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+            retrofit = retrofitBuilder();
+        } else {
+            okHttpClient = new OkHttpClient.Builder().build();
+            if (retrofit == null) {
+                retrofit = retrofitBuilder();
+            }
         }
         return retrofit;
+    }
+
+    private static Retrofit retrofitBuilder() {
+        return new Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
     }
 }
