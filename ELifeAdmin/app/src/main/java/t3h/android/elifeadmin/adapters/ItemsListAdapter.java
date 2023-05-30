0a8bf6871ebc605b86b5a88f51bb945a26c121b6
dkifashion <dkiwomanfashion@gmail.com>
2023-05-30 16:21:27 +0700

    handle base adapter for all object when show object list

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/adapters/ItemsListAdapter.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/adapters/ItemsListAdapter.java
@@ -1,2 +1,63 @@
-package t3h.android.elifeadmin.adapters;public class ItemsListAdapter {
+package t3h.android.elifeadmin.adapters;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import t3h.android.elifeadmin.databinding.ItemListLayoutBinding;
+import t3h.android.elifeadmin.listener.OnBindViewListener;
+
+public class ItemsListAdapter<T> extends RecyclerView.Adapter<ItemsListAdapter.ItemViewHolder> {
+    private List<T> data;
+    private OnBindViewListener<T> onBindViewListener;
+
+    public ItemsListAdapter() {
+        data = new ArrayList<>();
+    }
+
+    public void bindAdapter(OnBindViewListener<T> onBindViewListener) {
+        this.onBindViewListener = onBindViewListener;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder<T> onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ItemViewHolder<>(ItemListLayoutBinding.inflate(
+                LayoutInflater.from(parent.getContext())
+                , parent
+                , false
+        ));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        holder.bindView(data.get(position), onBindViewListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data != null ? data.size() : 0;
+    }
+
+    public static class ItemViewHolder<T> extends RecyclerView.ViewHolder {
+        ItemListLayoutBinding binding;
+
+        public ItemViewHolder(@NonNull ItemListLayoutBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bindView(T model, OnBindViewListener<T> listener) {
+            listener.onBindView(model, binding);
+        }
+    }
 }
--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/listener/OnBindViewListener.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/listener/OnBindViewListener.java
@@ -1,2 +1,7 @@
-package t3h.android.elifeadmin.listener;public interface OnBindViewListener {
+package t3h.android.elifeadmin.listener;
+
+import t3h.android.elifeadmin.databinding.ItemListLayoutBinding;
+
+public interface OnBindViewListener<T> {
+    void onBindView(T model, ItemListLayoutBinding view);
 }
