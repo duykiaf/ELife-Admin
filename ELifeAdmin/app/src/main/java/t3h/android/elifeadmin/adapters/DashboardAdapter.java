9c8976eb2472219f2d0022a85d01a7d7c1df43a1
dkifashion <dkiwomanfashion@gmail.com>
2023-05-30 16:18:48 +0700

    update: return ListFragment with position

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/adapters/DashboardAdapter.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/adapters/DashboardAdapter.java
@@ -1,13 +1,8 @@
 package t3h.android.elifeadmin.adapters;
 
-import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import t3h.android.elifeadmin.ui.ListFragment;
 
@@ -19,9 +14,7 @@ public class DashboardAdapter extends FragmentStateAdapter {
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        // Return a NEW fragment instance in createFragment(int)
-        Fragment fragment = new ListFragment();
-        return fragment;
+        return new ListFragment(position);
     }
 
     @Override
