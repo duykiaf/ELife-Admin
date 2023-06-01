e90157af977490dd6bf4b41d93ac5d3004e654a1
dkifashion <dkiwomanfashion@gmail.com>
2023-06-01 10:37:44 +0700

    create helper file to gennerate dropdown status list

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/helper/DropdownListHelper.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/helper/DropdownListHelper.java
@@ -1,2 +1,15 @@
-package t3h.android.elifeadmin.helper;public class DropdownListHelper {
+package t3h.android.elifeadmin.helper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import t3h.android.elifeadmin.models.DropdownItem;
+
+public class DropdownListHelper {
+    public static List<DropdownItem> statusDropdown() {
+        List<DropdownItem> status = new ArrayList<>();
+        status.add(new DropdownItem(0, "Inactive"));
+        status.add(new DropdownItem(1, "Active"));
+        return status;
+    }
 }
