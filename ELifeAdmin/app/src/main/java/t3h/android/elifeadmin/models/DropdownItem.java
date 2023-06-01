0297c8819eafce6157ac69e6b79ed76100ab5c68
dkifashion <dkiwomanfashion@gmail.com>
2023-06-01 10:36:19 +0700

    build dropdown item object

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/models/DropdownItem.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/models/DropdownItem.java
@@ -1,2 +1,36 @@
-package t3h.android.elifeadmin.models;public class DropdownItem {
+package t3h.android.elifeadmin.models;
+
+import androidx.annotation.NonNull;
+
+public class DropdownItem {
+    private int hiddenValue;
+
+    private String displayText;
+
+    public DropdownItem(int hiddenValue, String displayText) {
+        this.hiddenValue = hiddenValue;
+        this.displayText = displayText;
+    }
+
+    public int getHiddenValue() {
+        return hiddenValue;
+    }
+
+    public void setHiddenValue(int hiddenValue) {
+        this.hiddenValue = hiddenValue;
+    }
+
+    public String getDisplayText() {
+        return displayText;
+    }
+
+    public void setDisplayText(String displayText) {
+        this.displayText = displayText;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return displayText;
+    }
 }
