81f79b9e5a5708b8c9cb27393015fca57d507e73
dkifashion <dkiwomanfashion@gmail.com>
2023-06-06 14:54:07 +0700

    build callback to get data from api

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/callback/ResultListCallback.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/callback/ResultListCallback.java
@@ -1,2 +1,7 @@
-package t3h.android.elifeadmin.callback;public interface ResultListCallback {
+package t3h.android.elifeadmin.callback;
+
+import java.util.List;
+
+public interface ResultListCallback<T> {
+    void onResultList(List<T> resultList);
 }
