6171747bebf83e8a9df8b02e10159d95e22c17e9
dkifashion <dkiwomanfashion@gmail.com>
2023-06-07 22:15:46 +0700

    update value

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/constant/AppConstant.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/constant/AppConstant.java
@@ -18,7 +18,7 @@ public class AppConstant {
     public static final String REFRESH_TOKEN_KEY = "t3h.android.elifeadmin.REFRESH_TOKEN";
     public static final String REFRESH_TOKEN_SUCCESSFULLY = "refresh token successfully";
     public static final String SIGN_IN_AGAIN = "Please sign in again to use this feature!";
-    public static final String UPLOAD_IMAGE_FAILED = "Upload image failed. Please try again!";
+    public static final String UPLOAD_FILE_FAILED = "Upload file failed. Please try again!";
     public static final String IMAGE_DEFAULT =
             "https://firebasestorage.googleapis.com/v0/b/elife-e4985.appspot.com/o/elife_logo.png?alt=media&token=1c00288c-e4b5-4801-8001-86abdadba150&_gl=1*swxjga*_ga*MTk0ODkxMTk4Ni4xNjYxOTk1MzM1*_ga_CW55HF8NVT*MTY4NTk0OTY2Ni4xNS4xLjE2ODU5NTI5ODAuMC4wLjA.";
     public static final int INPUT_MAX_LENGTH = 50;
@@ -26,6 +26,7 @@ public class AppConstant {
     public static final int REQUEST_CODE_SELECT_IMAGE = 888;
     public static final String LIMIT_ERROR = "This field must not exceed 50 characters";
     public static final String MUST_NOT_BE_EMPTY = "This field must not be empty!";
+    public static final String NO_FILE = "Select audio file please!";
     public static final String IS_EXISTS_MESSAGE = "This name already exists";
     public static final String CREATE_SUCCESSFULLY = "Create successfully!";
     public static final String CREATE_FAILED = "Create failed. Please try again!";
@@ -34,4 +35,5 @@ public class AppConstant {
     public static final String EMPTY_ERROR = "Please complete all information";
     public static final String SIGN_FAILED = "Sign in failed. Please try again!";
     public static final String PRESS_AGAIN = "Press back again to exit";
+    public static final int REQUEST_CODE_SELECT_AUDIO = 666;
 }
