a792582731babea0026367563de35adb140e7ba9
dkifashion <dkiwomanfashion@gmail.com>
2023-06-10 15:23:35 +0700

    update search feature and write common private methods

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/ui/ListFragment.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/ui/ListFragment.java
@@ -19,6 +19,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import t3h.android.elifeadmin.R;
 import t3h.android.elifeadmin.adapters.ItemsListAdapter;
 import t3h.android.elifeadmin.databinding.FragmentListBinding;
+import t3h.android.elifeadmin.databinding.ItemListLayoutBinding;
 import t3h.android.elifeadmin.models.Audio;
 import t3h.android.elifeadmin.models.Category;
 import t3h.android.elifeadmin.models.Topic;
@@ -66,107 +67,30 @@ public class ListFragment extends Fragment {
             case 0:
                 ItemsListAdapter<Category> categoryAdapter = new ItemsListAdapter<>();
                 CategoryViewModel categoryViewModel = new ViewModelProvider(requireActivity()).get(CategoryViewModel.class);
-                categoryViewModel.getAllList().observe(requireActivity(), categories -> {
-                    categoryAdapter.setData(categories);
-                    fragmentListBinding.searchEdt.addTextChangedListener(new TextWatcher() {
-                        @Override
-                        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                        }
-
-                        @Override
-                        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                            categoryAdapter.cancelTimer();
-                        }
-
-                        @Override
-                        public void afterTextChanged(Editable editable) {
-                            categoryAdapter.searchList(editable.toString(), categories);
-                        }
-                    });
-                });
+                categoryViewModel.getAllList().observe(requireActivity(), categoryAdapter::setData);
                 fragmentListBinding.listRcv.setAdapter(categoryAdapter);
-                categoryAdapter.bindAdapter((model, view) -> {
-                    if (model.getStatus() == 0) {
-                        view.itemName.setTextColor(getResources().getColor(R.color.dangerColor));
-                        view.itemListLayout.setBackgroundResource(R.drawable.border_red_background);
-                    }
-                    view.itemName.setText(model.getName());
-                });
-                categoryAdapter.setOnItemListClickListener(object -> {
-                    bundle.putBoolean("isUpdate", true);
-                    bundle.putSerializable("categoryInfo", object);
-                    navController.navigate(R.id.action_dashboardFragment_to_createNewCategoryFragment, bundle);
-                });
+                categoryAdapter.bindAdapter(this::bindCategoryItemByStatus);
+                categoryAdapter.setOnItemListClickListener((object ->
+                        navigateToUpdateView(object, R.id.action_dashboardFragment_to_createNewCategoryFragment))
+                );
                 break;
             case 1:
                 ItemsListAdapter<Topic> topicAdapter = new ItemsListAdapter<>();
                 TopicViewModel topicViewModel = new ViewModelProvider(requireActivity()).get(TopicViewModel.class);
-                topicViewModel.getAllList().observe(requireActivity(), topics -> {
-                    topicAdapter.setData(topics);
-                    fragmentListBinding.searchEdt.addTextChangedListener(new TextWatcher() {
-                        @Override
-                        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                        }
-
-                        @Override
-                        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                            topicAdapter.cancelTimer();
-                        }
-
-                        @Override
-                        public void afterTextChanged(Editable editable) {
-                            topicAdapter.searchList(editable.toString(), topics);
-                        }
-                    });
-                });
+                topicViewModel.getAllList().observe(requireActivity(), topicAdapter::setData);
                 fragmentListBinding.listRcv.setAdapter(topicAdapter);
-                topicAdapter.bindAdapter((model, view) -> {
-                    if (model.getStatus() == 0) {
-                        view.itemName.setTextColor(getResources().getColor(R.color.dangerColor));
-                        view.itemListLayout.setBackgroundResource(R.drawable.border_red_background);
-                    }
-                    view.itemName.setText(model.getName());
-                });
-                topicAdapter.setOnItemListClickListener(object -> {
-                    bundle.putBoolean("isUpdate", true);
-                    bundle.putSerializable("topicInfo", object);
-                    navController.navigate(R.id.action_dashboardFragment_to_createNewTopicFragment, bundle);
-                });
+                topicAdapter.bindAdapter(this::bindTopicItemByStatus);
+                topicAdapter.setOnItemListClickListener((object ->
+                        navigateToUpdateView(object, R.id.action_dashboardFragment_to_createNewTopicFragment)));
                 break;
             case 2:
                 ItemsListAdapter<Audio> audioAdapter = new ItemsListAdapter<>();
                 AudioViewModel audioViewModel = new ViewModelProvider(requireActivity()).get(AudioViewModel.class);
-                audioViewModel.getAllList().observe(requireActivity(), audios -> {
-                    audioAdapter.setData(audios);
-                    fragmentListBinding.searchEdt.addTextChangedListener(new TextWatcher() {
-                        @Override
-                        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                        }
-
-                        @Override
-                        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                            audioAdapter.cancelTimer();
-                        }
-
-                        @Override
-                        public void afterTextChanged(Editable editable) {
-                            audioAdapter.searchList(editable.toString(), audios);
-                        }
-                    });
-                });
+                audioViewModel.getAllList().observe(requireActivity(), audioAdapter::setData);
                 fragmentListBinding.listRcv.setAdapter(audioAdapter);
-                audioAdapter.bindAdapter((model, view) -> {
-                    if (model.getStatus() == 0) {
-                        view.itemName.setTextColor(getResources().getColor(R.color.dangerColor));
-                        view.itemListLayout.setBackgroundResource(R.drawable.border_red_background);
-                    }
-                    view.itemName.setText(model.getTitle());
-                });
-                audioAdapter.setOnItemListClickListener(object -> {
-                    bundle.putBoolean("isUpdate", true);
-                    bundle.putSerializable("audioInfo", object);
-                    navController.navigate(R.id.action_dashboardFragment_to_createNewAudioFragment, bundle);
-                });
+                audioAdapter.bindAdapter(this::bindAudioItemByStatus);
+                audioAdapter.setOnItemListClickListener((object ->
+                        navigateToUpdateView(object, R.id.action_dashboardFragment_to_createNewAudioFragment)));
                 break;
         }
         fragmentListBinding.listRcv.setLayoutManager(new LinearLayoutManager(requireActivity()));
@@ -180,8 +104,130 @@ public class ListFragment extends Fragment {
             } else {
                 fragmentListBinding.searchEdt.setVisibility(View.VISIBLE);
                 fragmentListBinding.searchImageView.setBackgroundResource(R.drawable.pressed_button_background);
+
+                switch (position) {
+                    case 0:
+                        CategoryViewModel categoryViewModel = new ViewModelProvider(requireActivity()).get(CategoryViewModel.class);
+                        ItemsListAdapter<Category> searchListAdapter = new ItemsListAdapter<>();
+                        fragmentListBinding.searchEdt.addTextChangedListener(new TextWatcher() {
+                            @Override
+                            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                            }
+
+                            @Override
+                            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                                searchListAdapter.cancelTimer();
+                            }
+
+                            @Override
+                            public void afterTextChanged(Editable editable) {
+                                categoryViewModel.getAllList().observe(requireActivity(), categories -> {
+                                    searchListAdapter.setData(categories);
+                                    searchListAdapter.searchList(editable.toString(), categories);
+                                });
+                                fragmentListBinding.listRcv.setAdapter(searchListAdapter);
+                                searchListAdapter.bindAdapter((model, view) -> bindCategoryItemByStatus(model, view));
+                                searchListAdapter.setOnItemListClickListener(object ->
+                                        navigateToUpdateView(object, R.id.action_dashboardFragment_to_createNewCategoryFragment)
+                                );
+                            }
+                        });
+                        break;
+                    case 1:
+                        TopicViewModel topicViewModel = new ViewModelProvider(requireActivity()).get(TopicViewModel.class);
+                        ItemsListAdapter<Topic> searchTopicsListAdapter = new ItemsListAdapter<>();
+                        fragmentListBinding.searchEdt.addTextChangedListener(new TextWatcher() {
+                            @Override
+                            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                            }
+
+                            @Override
+                            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                                searchTopicsListAdapter.cancelTimer();
+                            }
+
+                            @Override
+                            public void afterTextChanged(Editable editable) {
+                                topicViewModel.getAllList().observe(requireActivity(), topics -> {
+                                    searchTopicsListAdapter.setData(topics);
+                                    searchTopicsListAdapter.searchList(editable.toString(), topics);
+                                });
+                                fragmentListBinding.listRcv.setAdapter(searchTopicsListAdapter);
+                                searchTopicsListAdapter.bindAdapter((model, view) -> bindTopicItemByStatus(model, view));
+                                searchTopicsListAdapter.setOnItemListClickListener(object ->
+                                        navigateToUpdateView(object, R.id.action_dashboardFragment_to_createNewTopicFragment)
+                                );
+                            }
+                        });
+                        break;
+                    case 2:
+                        AudioViewModel audioViewModel = new ViewModelProvider(requireActivity()).get(AudioViewModel.class);
+                        ItemsListAdapter<Audio> searchAudiosListAdapter = new ItemsListAdapter<>();
+                        fragmentListBinding.searchEdt.addTextChangedListener(new TextWatcher() {
+                            @Override
+                            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                            }
+
+                            @Override
+                            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                                searchAudiosListAdapter.cancelTimer();
+                            }
+
+                            @Override
+                            public void afterTextChanged(Editable editable) {
+                                audioViewModel.getAllList().observe(requireActivity(), audios -> {
+                                    searchAudiosListAdapter.setData(audios);
+                                    searchAudiosListAdapter.searchList(editable.toString(), audios);
+                                });
+                                fragmentListBinding.listRcv.setAdapter(searchAudiosListAdapter);
+                                searchAudiosListAdapter.bindAdapter((model, view) -> bindAudioItemByStatus(model, view));
+                                searchAudiosListAdapter.setOnItemListClickListener(object ->
+                                        navigateToUpdateView(object, R.id.action_dashboardFragment_to_createNewAudioFragment)
+                                );
+                            }
+                        });
+                        break;
+                }
             }
         });
+    }
+
+    private void bindCategoryItemByStatus(Category model, ItemListLayoutBinding view) {
+        if (model.getStatus() == 0) {
+            bindViewColor(view);
+        }
+        view.itemName.setText(model.getName());
+    }
+
+    private void bindTopicItemByStatus(Topic model, ItemListLayoutBinding view) {
+        if (model.getStatus() == 0) {
+            bindViewColor(view);
+        }
+        view.itemName.setText(model.getName());
+    }
+
+    private void bindAudioItemByStatus(Audio model, ItemListLayoutBinding view) {
+        if (model.getStatus() == 0) {
+            bindViewColor(view);
+        }
+        view.itemName.setText(model.getTitle());
+    }
+
+    private void bindViewColor(ItemListLayoutBinding view) {
+        view.itemName.setTextColor(getResources().getColor(R.color.dangerColor));
+        view.itemListLayout.setBackgroundResource(R.drawable.border_red_background);
+    }
+
+    private void navigateToUpdateView(Object object, int actionId) {
+        bundle.putBoolean("isUpdate", true);
+        if (object instanceof Category) {
+            bundle.putSerializable("categoryInfo", (Category) object);
+        } else if (object instanceof Topic) {
+            bundle.putSerializable("topicInfo", (Topic) object);
+        } else if (object instanceof Audio) {
+            bundle.putSerializable("audioInfo", (Audio) object);
+        }
+        navController.navigate(actionId, bundle);
     }
 
     @Override
