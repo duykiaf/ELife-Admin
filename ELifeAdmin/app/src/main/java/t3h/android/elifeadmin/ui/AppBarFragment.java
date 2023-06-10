a3c3e12c0f338b2a1c854ce3abc37eb4748a36fa
dkifashion <dkiwomanfashion@gmail.com>
2023-06-10 15:45:48 +0700

    handle menu items click event, but it do not work

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/ui/AppBarFragment.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/ui/AppBarFragment.java
@@ -9,9 +9,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import t3h.android.elifeadmin.R;
 import t3h.android.elifeadmin.databinding.FragmentAppBarBinding;
+import t3h.android.elifeadmin.helper.FirebaseAuthHelper;
 
 public class AppBarFragment extends Fragment {
     private FragmentAppBarBinding appBarBinding;
@@ -20,16 +23,34 @@ public class AppBarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         appBarBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_app_bar, container, false);
-        // Inflate the layout for this fragment
         return appBarBinding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Bundle args = requireArguments();
-        appBarBinding.topAppBar.setTitle(args.getString("PAGE_TITLE", "No page found"));
-        appBarBinding.topAppBar.setNavigationIcon(args.getInt("PAGE_ICON"));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        onMenuItemClick();
+    }
+
+    private void onMenuItemClick() {
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.navHostFragment);
+        appBarBinding.topAppBar.setOnMenuItemClickListener(menuItem -> {
+            switch (menuItem.getItemId()) {
+                case R.id.changePasswordItem:
+                    navController.navigate(R.id.changePasswordFragment);
+                    return true;
+                case R.id.logoutItem:
+                    FirebaseAuthHelper.signOut();
+                    navController.navigate(R.id.signInFragment);
+                    return true;
+            }
+            return false;
+        });
     }
 
     @Override
