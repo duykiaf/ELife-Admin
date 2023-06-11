7c1538cbe4aa498b6a7819b8214af4a4be929492
dkifashion <dkiwomanfashion@gmail.com>
2023-06-11 09:17:30 +0700

    handle decoded jwt

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/helper/JWTHelper.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/helper/JWTHelper.java
@@ -1,2 +1,32 @@
-package t3h.android.elifeadmin.helper;public class JWTHelper {
+package t3h.android.elifeadmin.helper;
+
+import android.util.Base64;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+public class JWTHelper {
+    public static String decoded(String refreshToken) {
+        String password;
+        String[] split = refreshToken.split("\\.");
+        try {
+            password = getPasswordFromJson(split[1]);
+        } catch (UnsupportedEncodingException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+        Log.e("Password", password);
+        return password;
+    }
+
+    private static String getPasswordFromJson(String strEncoded) throws UnsupportedEncodingException, JSONException {
+        byte[] decodedBytes = Base64.decode(strEncoded, Base64.URL_SAFE);
+        String decodedString = new String(decodedBytes, StandardCharsets.UTF_8);
+        JSONObject jsonObject = new JSONObject(decodedString);
+        JSONObject dataObject = jsonObject.getJSONObject("data");
+        return dataObject.getString("password");
+    }
 }
