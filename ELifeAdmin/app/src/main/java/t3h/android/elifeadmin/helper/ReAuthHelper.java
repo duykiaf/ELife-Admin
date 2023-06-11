3b8527d2376ae6d7d4505c8c1a0d1329b87eb7c5
dkifashion <dkiwomanfashion@gmail.com>
2023-06-11 17:03:32 +0700

    handle re-auth on another thread

--- ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/helper/ReAuthHelper.java
+++ ELifeAdmin/app/src/main/java/t3h/android/elifeadmin/helper/ReAuthHelper.java
@@ -4,26 +4,69 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.loader.content.AsyncTaskLoader;
 
 import t3h.android.elifeadmin.models.Account;
 import t3h.android.elifeadmin.viewmodels.TokenViewModel;
 
 public class ReAuthHelper {
-    public static void reAuth(String email, String password, SharedPreferences sharedPref, Activity activity) {
-        FirebaseAuthHelper.signIn(email, password, task -> {
-            if (task.isSuccessful()) {
-                TokenViewModel tokenViewModel = new ViewModelProvider((AppCompatActivity) activity).get(TokenViewModel.class);
-                tokenViewModel.createToken(new Account(email, password)).observe((AppCompatActivity) activity, token -> {
-                    if (!token.getAccessToken().trim().isEmpty()) {
-                        SharedPreferencesHelper.saveToken(sharedPref, token);
-                    }
-                    Log.e("re-auth", token.getMessage());
-                });
-            } else {
-                Log.e("re-auth", "re-auth failed");
+    public void reAuth(Activity activity, String email, String password,
+                       SharedPreferences sharedPref, OnFinishListener onFinishListener) {
+        new ReAuth(activity, email, password, sharedPref, onFinishListener).forceLoad();
+    }
+
+    public interface OnFinishListener {
+        void onFinish(boolean isSuccess);
+    }
+
+    private class ReAuth extends AsyncTaskLoader<Boolean> {
+        private final Activity activity;
+        private final String email;
+        private final String password;
+        private final SharedPreferences sharedPref;
+        private final OnFinishListener onFinishListener;
+
+        public ReAuth(@NonNull Activity activity, String email, String password,
+                      SharedPreferences sharedPref, OnFinishListener onFinishListener) {
+            super(activity);
+            this.activity = activity;
+            this.email = email;
+            this.password = password;
+            this.sharedPref = sharedPref;
+            this.onFinishListener = onFinishListener;
+        }
+
+        @Nullable
+        @Override
+        public Boolean loadInBackground() {
+            FirebaseAuthHelper.signIn(email, password, task -> {
+                if (task.isSuccessful()) {
+                    TokenViewModel tokenViewModel = new ViewModelProvider((AppCompatActivity) activity).get(TokenViewModel.class);
+                    tokenViewModel.createToken(new Account(email, password)).observe((AppCompatActivity) activity, token -> {
+                        if (!token.getAccessToken().trim().isEmpty()) {
+                            SharedPreferencesHelper.saveToken(sharedPref, token);
+                        }
+                        Log.e("re-auth", token.getMessage());
+                    });
+                    deliverResult(true);
+                } else {
+                    Log.e("re-auth", "re-auth failed");
+                    deliverResult(false);
+                }
+            });
+            return null;
+        }
+
+        @Override
+        public void deliverResult(@Nullable Boolean data) {
+            super.deliverResult(data);
+            if (onFinishListener != null) {
+                onFinishListener.onFinish(Boolean.TRUE.equals(data));
             }
-        });
+        }
     }
 }
